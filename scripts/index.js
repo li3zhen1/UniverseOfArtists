511317305551f840c588f9ad1f65a5a246770a9d
lizpku <master_lee@live.com>
2019-11-21 15:54:31 +0800

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
-document.body.addEventListener('touchmove', function(e){
-    e.preventDefault();
-}, { passive: false });
-var svg = d3.select("svg").attr('width', Math.max(window.innerWidth, 1600))
-width = +svg.attr("width"),
-    height = +svg.attr("height");
+document.body.addEventListener("touchmove", bodyScroll, false);
+function bodyScroll(event) {
+    event.preventDefault();
+}
 const colorSet = ['#ffcdd2', '#ff8a65', '#ffb300', '#ff9800', '#ff4081', '#d500f9', '#512da8', '#536dfe', '#2196f3',
     '#26c6da', '#00695c', '#1de9b6', '#00e676', '#ffeb3b', '#c6ff00', '#c51162', '#f4511e', '#ef5350', '#f48fb1', '#ffcc80', '#90a4ae', '#33691e']
+
+var svg = d3.select("svg").attr('width', Math.max(window.innerWidth, 1300))
+width = +svg.attr("width"),
+    height = +svg.attr("height");
 var forceGraph_Configs = {
     startRatio: 1,
     endRatio: 0.001,
@@ -27,17 +29,20 @@ function retOrig(x) {
 function index(d) {
     return d.index;
 }
+
 function find(nodeById, nodeId) {
     var node = nodeById.get(nodeId);
     if (!node) throw new Error("missing: " + nodeId);
     return node;
 }
+
 function renderForceDirectedGraph(nodes) {
     if (!nodes) nodes = [];
     var stepRatio = forceGraph_Configs.stepRatio;
     var stepper = d3.timer(step),
         field = d3.map(),
         event = d3.dispatch("tick", "end");
+
     function step() {
         tick();
         event.call("tick", forceGraph);
@@ -46,6 +51,7 @@ function renderForceDirectedGraph(nodes) {
             event.call("end", forceGraph);
         }
     }
+
     function tick() {
         forceGraph_Configs.startRatio += (forceGraph_Configs.stepRatio - forceGraph_Configs.startRatio) * forceGraph_Configs.damping_l;
         //if (forceGraph_Configs.startRatio < 0.18) forceGraph_Configs.startRatio = 0.3;
@@ -68,6 +74,7 @@ function renderForceDirectedGraph(nodes) {
                 node.y = node.fy, node.vy = 0;
         }
     }
+
     forceGraph = {
         nodes: function (tmp) {
             return (nodes = tmp, render_nodes(), field.each(render), forceGraph)
@@ -85,6 +92,7 @@ function renderForceDirectedGraph(nodes) {
             return stepper.restart(step), forceGraph;
         },
     };
+
     function render_nodes() {
         for (var i = 0, n = nodes.length, node; i < n; ++i) {
             node = nodes[i], node.index = i;
@@ -95,11 +103,13 @@ function renderForceDirectedGraph(nodes) {
             node.vx = node.vy = 0;
         }
     }
+
     function render(f) {
         if (f.initialize)
             f.initialize(nodes);
         return f;
     }
+
     for (var i = 0, n = nodes.length, node; i < n; ++i) {
         node = nodes[i], node.index = i;
         var radius = forceGraph_Configs.initialRadius * Math.sqrt(i),
@@ -108,19 +118,31 @@ function renderForceDirectedGraph(nodes) {
         node.y = radius * Math.sin(angle);
         node.vx = node.vy = 0;
     }
+
     return forceGraph;
 }
+
+
 var strengths;
 var strength;
 var distances;
+
+
 function calLink(links) {
+
     var id = index,
         distance,
         nodes,
         count,
         bias,
         iterations = 1;
+
     if (links == null) links = [];
+
+    function defaultStrength(link) {
+        return 1 / Math.min(count[link.source.index], count[link.target.index]);
+    }
+
     function force(alpha) {
         for (var k = 0, n = links.length; k < iterations; ++k) {
             for (var i = 0, link, source, target, x, y, l, b; i < n; ++i) {
@@ -159,12 +181,14 @@ function calLink(links) {
     }
     function initializeStrength() {
         if (!nodes) return;
+
         for (var i = 0, n = links.length; i < n; ++i) {
             strengths[i] = +strength(links[i], i, links);
         }
     }
     function initializeDistance() {
         if (!nodes) return;
+
         for (var i = 0, n = links.length; i < n; ++i) {
             distances[i] = +distance(links[i], i, links);
         }
@@ -185,10 +209,13 @@ function calLink(links) {
     return force;
 }
 var graphContainer = renderForceDirectedGraph();
+
+
 var graph = {
     nodes: [],
     links: []
 }
+var isChosen = false;
 var highlightedId = -1;
 d3.json("data/connections.json", function (error, data) {
     if (error) throw error;
@@ -197,12 +224,15 @@ d3.json("data/connections.json", function (error, data) {
         .force("center", d3.forceCenter(680, 420));
     graph = data;
     edgeNum = data.links.length;
+
+
     var nodes = graph.nodes,
         nodeById = d3.map(nodes, function (d) {
             return d.id;
         }),
         links = graph.links,
         bilinks = [];
+
     links.forEach(function (link) {
         var s = link.source = nodeById.get(link.source),
             t = link.target = nodeById.get(link.target),
@@ -217,11 +247,14 @@ d3.json("data/connections.json", function (error, data) {
         });
         bilinks.push([s, i, t]);
     });
+
     var link = svg.selectAll(".link")
         .data(bilinks)
         .enter().append("path")
         .attr("class", "link")
         .attr("id", function (e, i) { return "link" + i })
+    //.attr('stroke', 'rgba(190, 190, 190, .9)')
+
     var node = svg.selectAll(".node")
         .data(nodes.filter(function (d) {
             return d.id;
@@ -233,38 +266,31 @@ d3.json("data/connections.json", function (error, data) {
         .on('mouseover', function (d) {
             d3.select('.boxshadowedDiv2').classed('boxshadowedDiv2-moved', true);
             if (!lockedLink) {
-                var linkNum = 0;
-                var linkSet = [];
                 d3.select("#name" + d.id).attr('class', 'op1')
-
-                graph.links.slice(0, edgeNum).forEach(function (t, i) {
-                    if (t.source.id == d.id || t.target.id == d.id) {
-                        d3.select("#link" + i)
-                            .attr('class', 'link-highlight')
-                        linkNum++;
-                        if (linkSet.indexOf(t.source.id) == -1) linkSet.push(t.source.id)
-                        if (linkSet.indexOf(t.target.id) == -1) linkSet.push(t.target.id)
-                    } else {
-                        d3.select("#link" + i)
-                            .attr('class', 'link')
-                            .classed('link-hidden', true)
-                    }
-                })
+                var bacon = 0;
                 for (var i = 0; i < 85; i++) {
-                    if (linkSet.indexOf(i) == -1) {
-                        d3.select("#node" + i).classed('nodeShown', false)
-                            .classed('nodeHidden', true)
-                        d3.select('#circle' + i).classed('nodeHighlight', false)
+                    if (i == graph.nodes[d.id].connectedArtists[bacon]) {
+                        bacon++;
+                    }
+                    else if (i == d.id) {
+                        HighlightCenteredNode(i);
                     }
                     else {
-                        d3.select("#node" + i).classed('nodeHidden', false)
-                            .classed('nodeShown', true)
-                        d3.select('#circle' + i).classed('nodeHighlight', true)
-                        if (i == d.id) d3.select('#circle' + i).classed('nodedeCentered', false).classed('nodeCentered', true)
+                        FadeOutNode(i);
+                    }
+                }
+                bacon = 0;
+                for (var i = 0; i < edgeNum; i++) {
+                    if (i == graph.nodes[d.id].connections[bacon]) {
+                        bacon++;
+                        HighlightEdge(i);
+                    }
+                    else {
+                        FadeOutEdge(i);
                     }
                 }
                 d3.select('.imagefit').attr('src', 'resource/images/' + d.id + '.jpg')
-                document.getElementById('ArtistLink').innerHTML = linkNum;
+                document.getElementById('ArtistLink').innerHTML = graph.nodes[d.id].connections.length;
                 document.getElementById('ArtistName').innerHTML = graph.nodes[d.id].name;
                 document.getElementById('ArtistBirth').innerHTML = artistInfo[d.id].birth_date + ', in ' + artistInfo[d.id].birth_place;
                 document.getElementById('ArtistDeath').innerHTML = artistInfo[d.id].death_date + ', in ' + artistInfo[d.id].work_locations;
@@ -274,41 +300,50 @@ d3.json("data/connections.json", function (error, data) {
         .on('mouseout', function (d) {
             if (!lockedLink) {
                 d3.select("#name" + d.id).attr('class', 'op0')
-                graph.links.slice(0, edgeNum).forEach(function (t, i) {
-                    if (t.source.id == d.id || t.target.id == d.id)
-                        d3.select("#link" + i)
-                            .attr('class', 'link')
-                })
+                var bacon = 0;
                 for (var i = 0; i < 85; i++) {
-                    d3.select("#node" + i).classed('nodeHidden', false);
-                    d3.select("#node" + i).classed('nodeShown', true);
-                    d3.select('#circle' + i).classed('nodeHighlight', false)
-                    if (i == d.id) d3.select('#circle' + i).classed('nodeCentered', false).classed('nodedeCentered', true)
+                    if (i == graph.nodes[d.id].connectedArtists[bacon]) {
+                        bacon++;
+                    }
+                    else if (i == d.id) {
+                        De_HighlightCenteredNode(i);
+                    }
+                    else {
+                        FadeInNode(i);
+                    }
                 }
-                graph.links.slice(0, edgeNum).forEach(function (t, i) {
-                    d3.select("#link" + i)
-                        .attr('class', 'link')
-                        .classed('link-hidden', false)
-                })
+                bacon = 0;
+                for (var i = 0; i < edgeNum; i++) {
+                    if (i == graph.nodes[d.id].connections[bacon]) {
+                        bacon++;
+                        De_HighlightEdge(i);
+                    }
+                    else {
+                        FadeInEdge(i);
+                    }
+                }
             }
         })
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended))
+
     graphContainer
         .nodes(nodes)
         .on("tick", ticked);
+
     graphContainer.force("link")
         .links(links);
+
     function ticked() {
         link.attr("d", positionLink);
         node.attr("transform", positionNode);
     }
     for (var i = 0; i < 85; i++) {
         d3.select('#node' + i)
-            .append('circle').attr('r', '6px').attr('id', "circle" + i).attr("fill", colorSet[graph.nodes[i].group])
-            .classed('borderedNode', true)
+            .append('circle').attr('r', '6.2px')
+            .attr('id', "circle" + i).attr("fill", colorSet[graph.nodes[i].group])
         var ctn = d3.select('#node' + i)
             .append('g').attr('id', 'name' + i)
             .attr('transform', 'translate(' + -48 + ',' + 16 + ')')
@@ -322,9 +357,11 @@ d3.json("data/connections.json", function (error, data) {
             .attr('rx', '3px')
             .attr('ry', '3px')
         ctn.append('text').text(graph.nodes[i].name).attr('fill', 'black').attr('transform', 'translate(' + 12 + ',' + 17.8 + ')')
+
     }
 });
 var lockedLink;
+
 function positionLink(d) {
     if (!forceGraph_Configs.renderStraightLine)
         return "M" + d[0].x + "," + d[0].y +
@@ -333,9 +370,11 @@ function positionLink(d) {
     return "M" + d[0].x + "," + d[0].y +
         "L" + d[2].x + "," + d[2].y;
 }
+
 function positionNode(d) {
     return "translate(" + d.x + "," + d.y + ")";
 }
+
 function dragstarted(d) {
     lockedLink = true;
     if (!d3.event.active)
@@ -343,25 +382,73 @@ function dragstarted(d) {
     d.fx = d.x, d.fy = d.y;
     forceGraph_Configs.startRatio = 0.3;
 }
+
 function dragged(d) {
     d.fx = d3.event.x, d.fy = d3.event.y;
 }
+
 function dragended(d) {
     lockedLink = false;
-    for (var i = 0; i < edgeNum; i++)
-        d3.select("#link" + i)
-            .attr('class', 'link')
-    d3.select("#name" + d.id).attr('class', 'op0')
+
+    var bacon = 0;
     for (var i = 0; i < 85; i++) {
-        d3.select("#node" + i).classed('nodeHidden', false);
-        d3.select("#node" + i).classed('nodeShown', true);
-        d3.select('#circle' + i).classed('nodeCentered', false).classed('nodedeCentered', true)
+        if (i == graph.nodes[d.id].connectedArtists[bacon]) {
+            bacon++;
+        }
+        else if (i == d.id) {
+            De_HighlightCenteredNode(i);
+        }
+        else {
+            FadeInNode(i);
+        }
     }
+    bacon = 0;
+    for (var i = 0; i < edgeNum; i++) {
+        if (i == graph.nodes[d.id].connections[bacon]) {
+            bacon++;
+            De_HighlightEdge(i);
+        }
+        else {
+            FadeInEdge(i);
+        }
+    }
+    d3.select("#name" + d.id).attr('class', 'op0')
     if (!d3.event.active) graphContainer.stepRatio(0);
     d.fx = null, d.fy = null;
 }
+
 var artistInfo;
 d3.json('data/artists.json', function (error, data) {
     if (error) throw error;
     artistInfo = data;
 })
+
+
+function HighlightCenteredNode(id) {
+    d3.select("#circle" + id).classed('node-centered-', false).classed('node-centered', true)
+}
+
+function De_HighlightCenteredNode(id) {
+    d3.select("#circle" + id).classed('node-centered', false).classed('node-centered-', true)
+}
+
+function FadeOutNode(id) {
+    d3.select("#circle" + id).attr('opacity', 0.2);
+}
+function FadeInNode(id) {
+    d3.select("#circle" + id).attr('opacity', 1);
+}
+function HighlightEdge(id) {
+    d3.select("#link" + id).attr('class', 'link-highlighted');
+}
+
+function De_HighlightEdge(id) {
+    d3.select("#link" + id).attr('class', 'link-highlighted-to-normal');
+}
+
+function FadeOutEdge(id) {
+    d3.select("#link" + id).attr('opacity', .2);
+}
+function FadeInEdge(id) {
+    d3.select("#link" + id).attr('opacity', 1);
+}
